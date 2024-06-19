6378816169f2d0795910fa47ec9e84c45853c6df
Sudhanshu Ganesh A <Sudhanshu4net@gmail.com>
2024-06-19 20:28:04 +0530

    Video-app Complete

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/peerjs", peerServer);
 
 var nodemailer = require("nodemailer")
 const transporter = nodemailer.createTransport({
-    port : 587,
+    port : 465,
     host : "smtp.gmail.com",
     auth : {
         user : "Sudhanshu4net@gmail.com",
